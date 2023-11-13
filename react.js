f8a94eeec711461f16f99e63fe71405ba7aff594
Dinesh Katariya <thedineshk24@gmail.com>
2023-11-13 16:46:05 +0530

    iteration-2

--- react.js
+++ react.js
@@ -1,9 +1,10 @@
 function render(element, container) {
   const domElement = document.createElement(element.type);
   domElement.innerHTML = element.children;
-  domElement.setAttribute("class", element.props.className);
-  domElement.setAttribute("href", element.props.href);
-  domElement.setAttribute("target", element.props.target);
+  for(let prop in element.props) {
+    if(prop === "children") continue;
+    domElement.setAttribute(prop, element.props[prop]);
+  }
 
   container.appendChild(domElement);
 }
@@ -12,7 +13,7 @@ const reactElement = {
   type: "a",
   props: {
     className: "container",
-    href: "http://www.baidu.com",
+    href: "https://aws.amazon.com/",
     target: "_blank",
   },
   children: "Hello World",
